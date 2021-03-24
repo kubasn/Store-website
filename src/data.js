b5b7fb139b5c9ac437fb2f84e242b4af3de4edcb
Kuba Sosin <56204568+kubasn@users.noreply.github.com>
2021-03-24 20:13:40 +0100

    #1 commit - create  landing page

---
+++ src/data.js
@@ -0,0 +1,66 @@
+const data = {
+    products: [
+        {
+            _id:'1',
+            name:'Pustak ceramiczny',
+            image:'images/p1.jpg',
+            price:120,
+            brand:'Leier',
+            rating:4.5,
+            numReviews:10,
+            description:''
+        },
+        {
+            _id:'2',
+            name:'w',
+            image:'images/p1.jpg',
+            price:120,
+            brand:'Leier',
+            rating:4.5,
+            numReviews:10,
+            description:''
+        },
+        {
+            _id:'3',
+            name:'e',
+            image:'images/p1.jpg',
+            price:120,
+            brand:'Leier',
+            rating:4.5,
+            numReviews:10,
+            description:''
+        },
+        {
+            _id:'4',
+            name:' ceramiczny',
+            image:'images/p1.jpg',
+            price:120,
+            brand:'Leier',
+            rating:4.5,
+            numReviews:10,
+            description:''
+        },
+        {
+            _id:'5',
+            name:'Pustak ceczny',
+            image:'images/p1.jpg',
+            price:120,
+            brand:'Leier',
+            rating:4.5,
+            numReviews:10,
+            description:''
+        },
+        {
+            _id:'6',
+            name:'Pustakasd ceczny',
+            image:'images/p1.jpg',
+            price:120,
+            brand:'Leier',
+            rating:4.5,
+            numReviews:10,
+            description:''
+        },
+    ]
+}
+
+export default data;
