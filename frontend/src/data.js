945813df385bb8c1aad6ca579400f9d7b1f29279
Kuba Sosin <56204568+kubasn@users.noreply.github.com>
2021-03-30 05:13:17 -0400

    Add files via upload

---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products: [
+        {
+            _id:'1',
+            name:'Pustak ceramiczny',
+            image:'/images/p1.jpg',
+            price:120,
+            brand:'Leier',
+            rating:2.5,
+            numReviews:10,
+            countInStock:5,
+            description:'fajny produkt'
+        },
+        {
+            _id:'2',
+            name:'w',
+            image:'/images/p1.jpg',
+            price:120,
+            brand:'Leier',
+            rating:1.5,
+            numReviews:10,
+            countInStock:5,
+            description:'ello asd'
+        },
+        {
+            _id:'3',
+            name:'e',
+            image:'/images/p1.jpg',
+            price:120,
+            brand:'Leier',
+            rating:4.5,
+            countInStock:5,
+            numReviews:10,
+            description:'qqq'
+        },
+        {
+            _id:'4',
+            name:' ceramiczny',
+            image:'/images/p1.jpg',
+            price:120,
+            brand:'Leier',
+            rating:4.5,
+            countInStock:5,
+            numReviews:10,
+            description:'opis nowy'
+        },
+        {
+            _id:'5',
+            name:'Pustak ceczny',
+            image:'/images/p1.jpg',
+            price:120,
+            brand:'Leier',
+            rating:4.5,
+            countInStock:5,
+            numReviews:10,
+            description:'hello '
+        },
+        {
+            _id:'6',
+            name:'Pustakasd ceczny',
+            image:'/images/p1.jpg',
+            price:120,
+            brand:'Leier',
+            rating:4.5,
+            countInStock:0,
+            numReviews:10,
+            description:'brak opisu'
+        },
+    ]
+}
+
+export default data;
